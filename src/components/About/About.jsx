95ec79563500c4e4610c97280fe69d430fc61f7b
Jitenkoundinye2004 <jitenkoundinyecse@gmail.com>
2026-07-25 00:58:50 +0530

    chore: update Google Drive resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a 
-            href="https://drive.google.com/file/d/1Dm4DQkW634Z7A-xps6RvMQB9F4cftDH4/view?usp=sharing" 
+            href="https://drive.google.com/file/d/1HeopgViC8ZneyBsolvYpXAWiCBYUNfUZ/view?usp=sharing" 
             target='_blank' 
             rel='noopener noreferrer' 
             className='inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105 cursor-pointer text-center' 
