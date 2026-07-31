76c43f34ff752e7d45881b4696549f2549c557cb
Jiten Koundinye <jitenkoundinyecse@gmail.com>
2026-08-01 00:00:49 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a 
-            href="https://drive.google.com/file/d/1HeopgViC8ZneyBsolvYpXAWiCBYUNfUZ/view?usp=sharing" 
+            href="https://drive.google.com/file/d/1ADrFYf6RLst-b4aLDoW2wqFpOJLjCu_-/view?usp=sharing" 
             target='_blank' 
             rel='noopener noreferrer' 
             className='inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105 cursor-pointer text-center' 
